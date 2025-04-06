9796a77fc19d4f81fd708cebe960f849c154afa8
Takeshi Doi <141570948+Takdoi@users.noreply.github.com>
2025-04-06 20:14:37 -0300

    Update ParametrosInvalidosException.java
    inserido as boas práticas no código

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,5 +1,10 @@
 public class ParametrosInvalidosException extends Exception {
+
+    // Adicionando um serialVersionUID para evitar problemas de serialização (caso deseje serializar a exceção)
+    private static final long serialVersionUID = 1L;
+
+    // Construtor que recebe uma mensagem de erro e a passa para a classe pai (Exception)
     public ParametrosInvalidosException(String mensagem) {
-        super(mensagem);
+        super(mensagem); // Passa a mensagem de erro para a classe Exception
     }
 }
